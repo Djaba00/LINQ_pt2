449b935239de5032511a9276bcc154026ef7c64b
Djaba00 <shipelenko9@gmail.com>
2023-03-10 12:36:17 +0600

    refactoring

--- Classroom/Program.cs
+++ Classroom/Program.cs
@@ -4,28 +4,28 @@ using System.Linq;
  
 namespace Task15
 {
-   class Program
-   {
-       static void Main(string[] args)
-       {
-           var classes = new []
-           {
-               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
-               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
-               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
-           };
-           var allStudents = GetAllStudents(classes);
-         
-           Console.WriteLine(string.Join(" ", allStudents));
-       }
- 
-       static string[] GetAllStudents( Classroom[] classes )
-       {
-            string[] allStudents = (from students in classes
-            from student in students.Students
-            select student).ToArray();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        var classes = new []
+        {
+            new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+            new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+            new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+        };
+        var allStudents = GetAllStudents(classes);
 
-            return allStudents;
-       }
-   }
+        Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents( Classroom[] classes )
+        {
+        string[] allStudents = (from students in classes
+        from student in students.Students
+        select student).ToArray();
+
+        return allStudents;
+        }
+    }
 }
